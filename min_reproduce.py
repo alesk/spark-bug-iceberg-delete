8da4ec1ef423be175ddaf2e04fb8b45c96f92d73
Ales Kotnik <ales@viaduct.ai>
2023-07-21 13:51:04 +0200

    Improve clarity of the bug reproduction code

--- min_reproduce.py
+++ min_reproduce.py
@@ -2,7 +2,18 @@ import pyspark
 import tempfile
 
 def get_spark_session(derby_dir, warehouse_dir):
+    """
+    Get or create a Spark session with the necessary configurations for reproducing the bug.
+
+    Parameters:
+        derby_dir (str): Directory path for the temporary Derby database.
+        warehouse_dir (str): Directory path for the temporary Iceberg warehouse.
+
+    Returns:
+        pyspark.sql.SparkSession: The Spark session.
+    """
     conf_dict = {
+        # Spark and Iceberg configurations
         'spark.app.name': 'spark_bug_001',
         'spark.jars.packages': 'org.apache.iceberg:iceberg-spark-runtime-3.4_2.12:1.3.0',
         'spark.sql.catalog.iceberg': 'org.apache.iceberg.spark.SparkCatalog',
@@ -13,7 +24,7 @@ def get_spark_session(derby_dir, warehouse_dir):
         'spark.driver.host': 'localhost',
         'spark.driver.memory': '512m',
         'spark.sql.shuffle.partitions': '1',
-        'spark.default.parallelism': '1',  # the culprit
+        'spark.default.parallelism': '1',  # This configuration causes the bug
         'spark.rdd.compress': False,
         'spark.shuffle.compress': False,
         'spark.ui.enabled': False,
@@ -22,20 +33,25 @@ def get_spark_session(derby_dir, warehouse_dir):
 
     conf = pyspark.SparkConf().setAll(list(conf_dict.items()))
     return pyspark.sql.SparkSession.builder.config(conf=conf).getOrCreate()
-def reproduce_bug(spark):
 
-    # create spark table with nullable column
+def reproduce_bug(spark):
+    """
+    Reproduce the bug in Spark when using the `MERGE INTO ... WHEN MATCHED THEN DELETE` statement.
+
+    Parameters:
+        spark (pyspark.sql.SparkSession): The Spark session.
+    """
+    # Create a Spark DataFrame with a nullable column
     dst_df = spark.createDataFrame([(1, 'm'), (3, None), (1, None)], ['id', 'nullable_string'])
 
-    # create another table with ids to be deleted
+    # Create another DataFrame with ids to be deleted
     remove_list_df = spark.createDataFrame([(1, )], ['id'])
 
     remove_list_df.createOrReplaceTempView('remove_list')
     dst_df.writeTo('iceberg.default.dst').createOrReplace()
 
-
-    # Delete all rows where id is included in remove_spark
-    # If one of the rows has a column with null an exception is thrown
+    # Delete all rows where id is included in remove_list_df
+    # If one of the rows has a column with null, an exception is thrown
     #
     #   java.lang.NullPointerException: Cannot invoke "org.apache.spark.unsafe.types.UTF8String.getBaseObject()" because "input" is null
     #
@@ -56,6 +72,7 @@ def reproduce_bug(spark):
     #    """
 
     spark.sql(delete_sql).show()
+
 def main():
     with tempfile.TemporaryDirectory() as derby_dir:
         with tempfile.TemporaryDirectory() as warehouse_dir:
